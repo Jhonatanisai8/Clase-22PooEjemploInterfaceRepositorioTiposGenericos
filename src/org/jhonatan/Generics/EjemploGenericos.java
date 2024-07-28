021e72f768f2afd89ee7e06a6952bc6fefbd6284
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 17:55:51 -0500

    class EjemploGenericos: creacion de una lista de tipo clientes

--- src/org/jhonatan/Generics/EjemploGenericos.java
+++ src/org/jhonatan/Generics/EjemploGenericos.java
@@ -1,7 +1,20 @@
 package org.jhonatan.Generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jhonatan.pooInterface.Modelo.Cliente;
+
 public class EjemploGenericos {
     public static void main(String[] args) {
         System.out.println("EJEMPLOS CON TIPOS GENERICOS");
+
+        /* Creamos una lista de clientes */
+        List <Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente("Andres", "Gusman"));
+
+        Cliente  andres =  clientes.iterator().next();       
+
     }
+
 }
