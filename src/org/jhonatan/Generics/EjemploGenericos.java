06e66825af9223dba8df590ef354fb2294a65b90
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 17:22:00 -0500

    class EjemploGenericos

---
+++ src/org/jhonatan/Generics/EjemploGenericos.java
@@ -0,0 +1,7 @@
+package org.jhonatan.Generics;
+
+public class EjemploGenericos {
+    public static void main(String[] args) {
+        System.out.println("EJEMPLOS CON TIPOS GENERICOS");
+    }
+}
