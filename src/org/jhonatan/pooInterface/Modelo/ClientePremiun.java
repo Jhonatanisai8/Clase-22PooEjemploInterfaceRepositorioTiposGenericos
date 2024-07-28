45f2908ae88cc6c00e70c5dc080160525452efd4
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 18:25:04 -0500

    class ClientePremiun : añadiendo que implemente el metodo comparable

--- src/org/jhonatan/pooInterface/Modelo/ClientePremiun.java
+++ src/org/jhonatan/pooInterface/Modelo/ClientePremiun.java
@@ -1,7 +1,12 @@
 package org.jhonatan.pooInterface.Modelo;
 
-public class ClientePremiun extends Cliente {
+public class ClientePremiun extends Cliente implements Comparable {
     public ClientePremiun(String nombre, String apellido) {
         super(nombre, apellido);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
     }
 }
