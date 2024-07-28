329d8daa6730d937e204c95d233640f71bcce919
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 18:03:37 -0500

    class EjemploGenericos: primer metodo de tipo generico

--- src/org/jhonatan/Generics/EjemploGenericos.java
+++ src/org/jhonatan/Generics/EjemploGenericos.java
@@ -1,6 +1,7 @@
 package org.jhonatan.Generics;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.jhonatan.pooInterface.Modelo.Cliente;
@@ -10,11 +11,37 @@ public class EjemploGenericos {
         System.out.println("EJEMPLOS CON TIPOS GENERICOS");
 
         /* Creamos una lista de clientes */
-        List <Cliente> clientes = new ArrayList<>();
+        List<Cliente> clientes = new ArrayList<>();
         clientes.add(new Cliente("Andres", "Gusman"));
 
-        Cliente  andres =  clientes.iterator().next();       
+        Cliente andres = clientes.iterator().next();
+
+        /* Creamos arreglos */
+        Cliente[] clientesArreglo = { new Cliente("Ana", "Martinez"),
+                new Cliente("Juan", "Arevalo"),
+                new Cliente("David", "Paz") };
+
+        Integer[] enterosArreglo = { 1, 2, 34, 5, 6, 7, 8 };
+
+        /*LLamamos al metodo estatico de esta misma clase */
+        List<Cliente> clientesLista = fromArrayToList(clientesArreglo);
+        List<Integer> enterosLista = fromArrayToList(enterosArreglo);
+
+        /*mostramos */
+        System.out.println("Lista de clientes: \n");
+        clientesLista.forEach(System.out::println);
+
+        System.out.println("Lista de enteros\n");
+        enterosLista.forEach(System.out::println);
+        
+        
 
     }
 
+    /* metodos para convertir un arreglo a lista */
+
+    /* indicamos con el <T> que es de tipo generico */
+    public static <T> List<T> fromArrayToList(T[] c) {
+        return Arrays.asList(c);
+    }
 }
