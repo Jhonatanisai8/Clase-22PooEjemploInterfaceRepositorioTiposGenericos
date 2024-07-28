91b66107b692f5ce57b27098b4049ce6d118a12b
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-28 18:14:52 -0500

    class ClientePremiun extends Cliente: nueva clase

---
+++ src/org/jhonatan/pooInterface/Modelo/ClientePremiun.java
@@ -0,0 +1,7 @@
+package org.jhonatan.pooInterface.Modelo;
+
+public class ClientePremiun extends Cliente {
+    public ClientePremiun(String nombre, String apellido) {
+        super(nombre, apellido);
+    }
+}
