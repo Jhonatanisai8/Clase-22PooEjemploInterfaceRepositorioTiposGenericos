3bfd1385aba2cc2ff42f90df7336d97120c823b4
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-31 10:06:20 -0500

    class EjemploGenericos: nuevo metodo para saber el maximo de tres objetos

--- src/org/jhonatan/Generics/EjemploGenericos.java
+++ src/org/jhonatan/Generics/EjemploGenericos.java
@@ -97,9 +97,23 @@ public class EjemploGenericos {
         return Arrays.asList(c);
     }
 
-    /*ahora puede imprimir tanto clientes y clientes premiun */
+    /* ahora puede imprimir tanto clientes y clientes premiun */
     public static void imprimirClientes(List<? extends Cliente> clientes) {
         clientes.forEach(System.out::println);
+    }
+
+    /* metodo de tipo generico para encontrar el maximo de tres objetos */
+    public static <T extends Comparable<T>> T maximo(T a, T b, T c) {
+        T max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+
+        return max;
     }
 
 }
