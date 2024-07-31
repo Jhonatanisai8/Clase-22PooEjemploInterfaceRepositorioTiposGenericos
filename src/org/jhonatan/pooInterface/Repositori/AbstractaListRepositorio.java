58864073e9b111eed445a0e6ec1c695eb6b07b0f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-31 15:34:29 -0500

    abstract class AbstractaListRepositorio<T>:  borrando los metodos comentados

--- src/org/jhonatan/pooInterface/Repositori/AbstractaListRepositorio.java
+++ src/org/jhonatan/pooInterface/Repositori/AbstractaListRepositorio.java
@@ -34,55 +34,16 @@ public abstract class AbstractaListRepositorio<T> implements OrdenablePaginableC
         this.dataSource.add(t);
     }
 
-    // public void editar(Cliente cliente) {
-    // Cliente c = this.porId(cliente.getId());
-    // c.setNombre(cliente.getNombre());
-    // c.setApellido(cliente.getApellido());
-    // }
-
     @Override
     public void eliminar(Integer id) {
         // Cliente c = ;this.porId(id)
         this.dataSource.remove(this.porId(id));
     }
 
-    // @Override
-    // public List<Cliente> listar(String campo, Direccion direccion) {
-    // List<Cliente> listOOrdenada = new ArrayList<>(this.dataSource);
-    // listOOrdenada.sort((a, b) -> {
-    // int result = 0;
-    // if (direccion == Direccion.ASC) {
-    // result = ordenar(campo, a, b);
-    // } else if (direccion == Direccion.DESD) {
-    // result = ordenar(campo, b, a);
-    // }
-    // return result;
-
-    // });
-    // return listOOrdenada;
-    // }
-
     @Override
     public List<T> listar(int desde, int hasta) {
         return this.dataSource.subList(desde, hasta);
     }
-
-    // static int ordenar(String campo, Cliente a, Cliente b) {
-
-    // int result = 0;
-    // switch (campo) {
-    // case "id":
-    // result = a.getId().compareTo(b.getId());
-    // break;
-    // case "nombre":
-    // result = a.getNombre().compareTo(b.getNombre());
-    // break;
-    // case "apellido":
-    // result = a.getApellido().compareTo(b.getApellido());
-    // break;
-    // }
-    // return result;
-    // }
 
     @Override
     public int total() {
