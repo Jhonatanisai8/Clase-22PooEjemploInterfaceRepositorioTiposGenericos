934cec458374dca54d4cb70e4b957503914e754f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-31 15:25:09 -0500

    class ClienteListRepositorio : tiene sus propios metodos, es clase hija de AbstractaListRepositorio<Cliente>

---
+++ src/org/jhonatan/pooInterface/Repositori/ClienteListRepositorio.java
@@ -0,0 +1,62 @@
+package org.jhonatan.pooInterface.Repositori;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jhonatan.pooInterface.Modelo.Cliente;
+
+public class ClienteListRepositorio extends AbstractaListRepositorio<Cliente> {
+
+    @Override
+    public Cliente porId(Integer id) {
+        Cliente c = null;
+        for (Cliente cliente : dataSource) {
+            if (cliente.getId() != null && cliente.getId().equals(id)) {
+                c = cliente;
+                break;
+            }
+        }
+        return c;
+    }
+
+    @Override
+    public void editar(Cliente cliente) {
+        Cliente c = this.porId(cliente.getId());
+        c.setNombre(cliente.getNombre());
+        c.setApellido(cliente.getApellido());
+    }
+
+    @Override
+    public List<Cliente> listar(String campo, Direccion direccion) {
+        List<Cliente> listOOrdenada = new ArrayList<>(this.dataSource);
+        listOOrdenada.sort((a, b) -> {
+            int result = 0;
+            if (direccion == Direccion.ASC) {
+                result = ordenar(campo, a, b);
+            } else if (direccion == Direccion.DESD) {
+                result = ordenar(campo, b, a);
+            }
+            return result;
+
+        });
+        return listOOrdenada;
+    }
+
+    static int ordenar(String campo, Cliente a, Cliente b) {
+
+        int result = 0;
+        switch (campo) {
+            case "id":
+                result = a.getId().compareTo(b.getId());
+                break;
+            case "nombre":
+                result = a.getNombre().compareTo(b.getNombre());
+                break;
+            case "apellido":
+                result = a.getApellido().compareTo(b.getApellido());
+                break;
+        }
+        return result;
+    }
+
+}
