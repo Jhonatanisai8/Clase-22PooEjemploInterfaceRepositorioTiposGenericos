a271ff1766b2fec21663375bc82ceba363192c48
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-07-31 15:15:36 -0500

    abstract class AbstractaListRepositorio: modifcando los demas metodos

--- src/org/jhonatan/pooInterface/Repositori/AbstractaListRepositorio.java
+++ src/org/jhonatan/pooInterface/Repositori/AbstractaListRepositorio.java
@@ -3,8 +3,6 @@ package org.jhonatan.pooInterface.Repositori;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jhonatan.pooInterface.Modelo.Cliente;
-
 public abstract class AbstractaListRepositorio<T> implements OrdenablePaginableCrud<T> {
 
     protected List<T> dataSource;
@@ -21,26 +19,26 @@ public abstract class AbstractaListRepositorio<T> implements OrdenablePaginableC
 
     // @Override
     // public Cliente porId(Integer id) {
-    //     Cliente c = null;
-    //     for (Cliente cliente : dataSource) {
-    //         if (cliente.getId() != null && cliente.getId().equals(id)) {
-    //             c = cliente;
-    //             break;
-    //         }
-    //     }
-    //     return c;
+    // Cliente c = null;
+    // for (Cliente cliente : dataSource) {
+    // if (cliente.getId() != null && cliente.getId().equals(id)) {
+    // c = cliente;
+    // break;
+    // }
+    // }
+    // return c;
     // }
 
     @Override
-    public void crear(Cliente cliente) {
-        this.dataSource.add(cliente);
+    public void crear(T t) {
+        this.dataSource.add(t);
     }
 
-    public void editar(Cliente cliente) {
-        Cliente c = this.porId(cliente.getId());
-        c.setNombre(cliente.getNombre());
-        c.setApellido(cliente.getApellido());
-    }
+    // public void editar(Cliente cliente) {
+    // Cliente c = this.porId(cliente.getId());
+    // c.setNombre(cliente.getNombre());
+    // c.setApellido(cliente.getApellido());
+    // }
 
     @Override
     public void eliminar(Integer id) {
@@ -48,43 +46,43 @@ public abstract class AbstractaListRepositorio<T> implements OrdenablePaginableC
         this.dataSource.remove(this.porId(id));
     }
 
-    @Override
-    public List<Cliente> listar(String campo, Direccion direccion) {
-        List<Cliente> listOOrdenada = new ArrayList<>(this.dataSource);
-        listOOrdenada.sort((a, b) -> {
-            int result = 0;
-            if (direccion == Direccion.ASC) {
-                result = ordenar(campo, a, b);
-            } else if (direccion == Direccion.DESD) {
-                result = ordenar(campo, b, a);
-            }
-            return result;
+    // @Override
+    // public List<Cliente> listar(String campo, Direccion direccion) {
+    // List<Cliente> listOOrdenada = new ArrayList<>(this.dataSource);
+    // listOOrdenada.sort((a, b) -> {
+    // int result = 0;
+    // if (direccion == Direccion.ASC) {
+    // result = ordenar(campo, a, b);
+    // } else if (direccion == Direccion.DESD) {
+    // result = ordenar(campo, b, a);
+    // }
+    // return result;
 
-        });
-        return listOOrdenada;
-    }
+    // });
+    // return listOOrdenada;
+    // }
 
     @Override
-    public List<Cliente> listar(int desde, int hasta) {
+    public List<T> listar(int desde, int hasta) {
         return this.dataSource.subList(desde, hasta);
     }
 
-    static int ordenar(String campo, Cliente a, Cliente b) {
+    // static int ordenar(String campo, Cliente a, Cliente b) {
 
-        int result = 0;
-        switch (campo) {
-            case "id":
-                result = a.getId().compareTo(b.getId());
-                break;
-            case "nombre":
-                result = a.getNombre().compareTo(b.getNombre());
-                break;
-            case "apellido":
-                result = a.getApellido().compareTo(b.getApellido());
-                break;
-        }
-        return result;
-    }
+    // int result = 0;
+    // switch (campo) {
+    // case "id":
+    // result = a.getId().compareTo(b.getId());
+    // break;
+    // case "nombre":
+    // result = a.getNombre().compareTo(b.getNombre());
+    // break;
+    // case "apellido":
+    // result = a.getApellido().compareTo(b.getApellido());
+    // break;
+    // }
+    // return result;
+    // }
 
     @Override
     public int total() {
